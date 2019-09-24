0eadd6e7ef67564e3d9847dfabd5bb2e7ee950fc
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-24 10:35:10 -0500

    nav bar and footer set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
+        <Nav />
         <h1>Confetti Blog</h1>
+        <Footer />
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <h6>hello i am the footer</h6>
+    )
+}
+
+export default Footer

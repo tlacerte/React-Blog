047ee0c8f2be66e455b4988ec00b2a675a5ec50d
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-25 10:32:04 -0500

    render posts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Nav from './Nav'
 import Footer from './Footer'
 
-function App() {
-  return (
-    <div className="App">
-        <Nav />
-        <h1>Confetti Blog</h1>
-        <Footer />
-    </div>
-  );
+class App extends Component {
+  state = {
+    isShowing : true,
+    posts : [
+      {
+        title : "My first confetti blog",
+        content : "i love confetti! it should be everywhere!!!!",
+        user : "tlacerte"
+      },
+      {
+        title : "blogs are fun",
+        content : "i've learned so much about blogs",
+        user : "sarahl"
+      }
+    ]
+  }
+  //define all event logic hear
+  handleClick = (event) => {
+    this.setState({
+      isShowing: !this.state.isShowing
+    })
+  }
+
+  // render will handle the view
+  render (){
+    //compose components down here and later we will pull these out
+    //TODO: extract to separate components 
+    const title = <h1>Confetti Blog</h1>
+    const composedPosts = this.state.posts.map((item, index) => {
+      return (
+        <li key={index} className="post">
+          <h3 clasName="postTitle">{item.title}</h3>
+          <p>{item.content}</p>
+          <h6>{item.user}</h6>
+        </li>
+      )
+    })
+    return (
+      <div className="App container">
+          <Nav />
+          {this.state.isShowing ? title : null}
+          <ul>{composedPosts}</ul>
+          <button onClick={this.handleClick}>click me</button>
+          <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <h6>hello i am the footer</h6>
+        <h6 className="footer">hello i am the footer</h6>
     )
 }
 
